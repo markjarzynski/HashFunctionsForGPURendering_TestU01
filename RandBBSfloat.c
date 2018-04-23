0e94d535d46e8506c8e9683bbaa711a260b0dacf
Mark Jarzynski <markj1@umbc.edu>
2018-04-23 15:27:58 -0400

    Added RandBBSfloat.

---
+++ RandBBSfloat.c
@@ -0,0 +1,28 @@
+#include <unif01.h>
+#include <bbattery.h>
+#include <math.h>
+
+#define BBS_PRIME24 4093
+
+// RNG definition meeting Test01 rules
+double RandBBSfloat() {
+    static double seed = 0;
+    double s = seed;
+    ++seed;
+
+    double i;
+
+    s = modf(s / BBS_PRIME24, &i);
+    s = modf(s * s * BBS_PRIME24, &i);
+    s = modf(s * s * BBS_PRIME24, &i);
+
+    return s;
+}
+
+// test harness
+int main() {
+    unif01_Gen *gen = unif01_CreateExternGen01("RandBBSfloat", RandBBSfloat);
+    bbattery_SmallCrush(gen);
+    unif01_DeleteExternGen01(gen);
+    return 0;
+}
