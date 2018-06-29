8130d35a4d1f9687fa286593290ac7402960381a
Mark Jarzynski <markj1@umbc.edu>
2018-06-29 02:15:12 -0400

    Added Ranlim32 from the Numerical Recipes book.

---
+++ Ranlim32.c
@@ -0,0 +1,42 @@
+#include <unif01.h>
+#include <bbattery.h>
+
+// RNG definition meeting Test01 rules
+uint32_t Ranlim32() {
+    static uint32_t seed = 0;
+    uint32_t j = seed;
+    ++seed;
+
+    uint32_t u, v, w1, w2, x, y;
+
+    v = 2244614371U;
+    w1 = 521288629U;
+    w2 = 362436069U;
+
+    u = j ^ v;
+
+    u = u * 2891336453U + 1640531513U;
+    v ^= v >> 13; v ^= v << 17; v ^= v >> 5;
+    w1 = 33378 * (w1 & 0xffff) + (w1 >> 16);
+    w2 = 57225 * (w2 & 0xffff) + (w2 >> 16);
+
+    v = u;
+
+    u = u * 2891336453U + 1640531513U;
+    v ^= v >> 13; v ^= v << 17; v ^= v >> 5;
+    w1 = 33378 * (w1 & 0xffff) + (w1 >> 16);
+    w2 = 57225 * (w2 & 0xffff) + (w2 >> 16);
+
+    x = u ^ (u << 9); x ^= x >> 17; x ^= x << 6;
+    y = w1 ^ (w1 << 17); y ^= y >> 15; y ^= y << 5;
+
+    return (x + v) ^ (y + w2);
+}
+
+// test harness
+int main() {
+    unif01_Gen *gen = unif01_CreateExternGenBits("Ranlim32", Ranlim32);
+    bbattery_SmallCrush(gen);
+    unif01_DeleteExternGen01(gen);
+    return 0;
+}
