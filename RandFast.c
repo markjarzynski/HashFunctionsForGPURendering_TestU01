d5ef83397ddff9a026c9f542670d5b9c8b0103bb
Mark Jarzynski <markj1@umbc.edu>
2018-08-01 17:51:08 -0400

    Fixed seed for RandFast

--- RandFast.c
+++ RandFast.c
@@ -1,28 +1,32 @@
 #include <unif01.h>
 #include <bbattery.h>
 #include <math.h>
+#include "morton.h"
+#include "util.h"
 
-static uint32_t x = 2463534242u;
-static uint32_t y = 12345u;
+static uint32_t seed = 0U;
 
 // RNG definition meeting Test01 rules
 double RandFast() {
-    double i;
+
+    uint32_t x, y;
+
+    vec2 v = unmorton2(seed);
 
     double Magic = 3571.0;
 
     x = (1.0 / 4320.0) * x + 0.25;
     y = (1.0 / 4320.0) * y;
 
-    double Random = modf((x * x + y * y) * Magic, &i);
+    double Random = frac((x * x + y * y) * Magic);
 
-    return modf(Random * Random * 2 * Magic, &i);
+    return frac(Random * Random * 2 * Magic);
 }
 
 // test harness
 int main() {
     unif01_Gen *gen = unif01_CreateExternGen01("RandFast", RandFast);
-    bbattery_SmallCrush(gen);
+    bbattery_BigCrush(gen);
     unif01_DeleteExternGen01(gen);
     return 0;
 }
