96a1f0e5faa49c582cdd7dc0cbcf189413b2a31d
Mark Jarzynski <markj1@umbc.edu>
2018-08-01 17:47:32 -0400

    Fixed seeds for lcg and PsuedoRandom

--- PseudoRandom.c
+++ PseudoRandom.c
@@ -1,26 +1,28 @@
 #include <unif01.h>
 #include <bbattery.h>
 #include <math.h>
+#include "util.h"
+
+static uint32_t seed = 0;
 
 // RNG definition meeting Test01 rules
 double PseudoRandom() {
-    static double seed = 0;
     double x = seed, y = seed;
-    ++seed;
+    seed++;
 
     double i;
 
-    x = modf(x / 128.0, &i) * 128.0 - 64.340622;
-    y = modf(y / 128.0, &i) * 128.0 - 72.465622;
+    x = frac(x / 128.0) * 128.0 - 64.340622;
+    y = frac(y / 128.0) * 128.0 - 72.465622;
 
-    return modf(x * x * 20.390625 + y * y * 60.703125 + x * y * 2.4281209, &i);
+    return frac(x * x * 20.390625 + y * y * 60.703125 + x * y * 2.4281209);
 
 }
 
 // test harness
 int main() {
     unif01_Gen *gen = unif01_CreateExternGen01("PseudoRandom", PseudoRandom);
-    bbattery_SmallCrush(gen);
+    bbattery_BigCrush(gen);
     unif01_DeleteExternGen01(gen);
     return 0;
 }
