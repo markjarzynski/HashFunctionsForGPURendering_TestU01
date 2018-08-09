115e7e1d1e30fa7d766bca83bb679552ba74aceb
Mark Jarzynski <markj1@umbc.edu>
2018-08-09 16:02:17 -0400

    added uint2 and uint3

---
+++ uint2.h
@@ -0,0 +1,97 @@
+#ifndef UINT3_H
+#define UINT3_H
+
+#include <cstdint>
+#include <math.h>
+
+typedef uint22_t uint;
+
+class uint2
+{
+public:
+   uint x, y;
+
+public:
+   uint2 () {
+       x = y = 0;
+   }
+
+   uint2 (uint x, uint y) {
+       this->x = x; this->y = y;
+   }
+};
+
+// vector addition
+inline uint2 operator + (const uint2 &a, const uint2 &b)
+{
+    return uint2(a.x + b.x, a.y + b.y);
+}
+
+// vector subtraction
+inline uint2 operator - (const uint2 &a, const uint2 &b)
+{
+    return uint2(a.x - b.x, a.y - b.y);
+}
+
+// scalar addition
+inline uint2 operator + (const uint2 &a, uint s)
+{
+    return uint2(a.x + s, a.y + s);
+}
+
+inline uint2 operator + (uint s, const uint2 &a)
+{
+    return uint2(s + a.x, s + a.y);
+}
+
+// scalar subtraction
+inline uint2 operator - (const uint2 &a, uint s)
+{
+    return uint2(a.x - s, a.y - s);
+}
+
+inline uint2 operator - (uint s, const uint2 &a)
+{
+    return uint2(s - a.x, s - a.y);
+}
+
+// vector multiplication
+inline uint2 operator * (const uint2 &a, const uint2 &b)
+{
+    return uint2(a.x * b.x, a.y * b.y);
+}
+
+// vector division
+inline uint2 operator / (const uint2 &a, const uint2 &b)
+{
+    return uint2(a.x / b.x, a.y / b.y);
+}
+
+// scalar multiplication
+inline uint2 operator * (const uint2 &a, uint s)
+{
+    return uint2(a.x * s, a.y * s);
+}
+
+inline uint2 operator * (uint s, const uint2 &a)
+{
+    return uint2(s * a.x, s * a.y);
+}
+
+// scalar division
+inline uint2 operator / (const uint2 &a, uint s)
+{
+    return uint2(a.x / s, a.y / s);
+}
+
+inline uint2 operator / (const uint s, const uint2 &a)
+{
+    return uint2(s / a.x, s / a.y);
+}
+
+inline uint dot (const uint2 &a, const uint2 &b)
+{
+    return a.x * b.x + a.y * b.y;
+}
+
+#endif
---
+++ uint3.h
@@ -0,0 +1,97 @@
+#ifndef UINT3_H
+#define UINT3_H
+
+#include <cstdint>
+#include <math.h>
+
+typedef uint32_t uint;
+
+class uint3
+{
+public:
+   uint x, y, z;
+
+public:
+   uint3 () {
+       x = y = z = 0;
+   }
+
+   uint3 (uint x, uint y, uint z) {
+       this->x = x; this->y = y; this->z = z;
+   }
+};
+
+// vector addition
+inline uint3 operator + (const uint3 &a, const uint3 &b)
+{
+    return uint3(a.x + b.x, a.y + b.y, a.z + b.z);
+}
+
+// vector subtraction
+inline uint3 operator - (const uint3 &a, const uint3 &b)
+{
+    return uint3(a.x - b.x, a.y - b.y, a.z - b.z);
+}
+
+// scalar addition
+inline uint3 operator + (const uint3 &a, uint s)
+{
+    return uint3(a.x + s, a.y + s, a.z + s);
+}
+
+inline uint3 operator + (uint s, const uint3 &a)
+{
+    return uint3(s + a.x, s + a.y, s + a.z);
+}
+
+// scalar subtraction
+inline uint3 operator - (const uint3 &a, uint s)
+{
+    return uint3(a.x - s, a.y - s, a.z - s);
+}
+
+inline uint3 operator - (uint s, const uint3 &a)
+{
+    return uint3(s - a.x, s - a.y, s - a.z);
+}
+
+// vector multiplication
+inline uint3 operator * (const uint3 &a, const uint3 &b)
+{
+    return uint3(a.x * b.x, a.y * b.y, a.z * b.z);
+}
+
+// vector division
+inline uint3 operator / (const uint3 &a, const uint3 &b)
+{
+    return uint3(a.x / b.x, a.y / b.y, a.z / b.z);
+}
+
+// scalar multiplication
+inline uint3 operator * (const uint3 &a, uint s)
+{
+    return uint3(a.x * s, a.y * s, a.z * s);
+}
+
+inline uint3 operator * (uint s, const uint3 &a)
+{
+    return uint3(s * a.x, s * a.y, s * a.z);
+}
+
+// scalar division
+inline uint3 operator / (const uint3 &a, uint s)
+{
+    return uint3(a.x / s, a.y / s, a.z / s);
+}
+
+inline uint3 operator / (const uint s, const uint3 &a)
+{
+    return uint3(s / a.x, s / a.y, s / a.z);
+}
+
+inline uint dot (const uint3 &a, const uint3 &b)
+{
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+#endif
