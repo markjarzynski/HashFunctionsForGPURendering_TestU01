8d9d1ce0b9ab398abc549063b5a635db6c336673
Mark Jarzynski <markj1@umbc.edu>
2018-11-19 19:18:45 -0500

    Updated scatter.py

--- scatter.py
+++ scatter.py
@@ -1,8 +1,31 @@
-#!/usr/bin/python
+#!/usr/local/bin/python3
 import csv
+import matplotlib.pyplot as plt
 
-with open('data.csv', 'rb') as f:
+with open('data.csv', 'r') as f:
     reader = csv.reader(f)
     l = list(reader)
+    
+name = []
+speed = []
+crush = []
 
-print(l)
+for i in l[1:]:
+    name.append(i[0])
+    speed.append(float(i[1]))
+    crush.append(int(i[2]))
+
+print(name)
+print(speed)
+print(crush)
+
+fig, ax = plt.subplots()
+ax.scatter(speed, crush)
+
+plt.xlabel(l[0][1])
+plt.ylabel(l[0][2])
+
+for i, txt in enumerate(name):
+    ax.annotate(txt, (speed[i], crush[i]), xytext=(2, 12), textcoords='offset pixels', horizontalalignment='left', verticalalignment='top', fontsize=8)
+
+plt.savefig('image.png')
